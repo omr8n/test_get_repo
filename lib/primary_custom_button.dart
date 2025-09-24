943b1543837eab68dd20489d69eef549c41c5d9d
Omran Abo Ali <omranaboaliii@gmail.com>
2025-09-24 15:26:54 +0300

    add customm  primary button

---
+++ lib/primary_custom_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PrimaryCustomButton extends StatelessWidget {
+  const PrimaryCustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
